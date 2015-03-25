4d3950319a22acba5bcffa1a286a14c25146b951
Riley Reuvers <rreuvers@nd.edu>
2015-03-25 12:48:21 -0400

    Fixed formatting, reviewed image code

--- main.cpp
+++ main.cpp
@@ -20,19 +20,19 @@ SDL_Window* gWindow = NULL; //The window we'll be rendering to
 SDL_Surface* gScreenSurface = NULL; //The surface contained by the window
 SDL_Surface* gImage = NULL;//The image we will load and show on the screen
 
-bool init(){
+bool init() {
 	bool success = true;//Initialization flag
-	
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){ //Initialize SDL
+
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) { //Initialize SDL
 		cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
 		success = false;
-	}else{ //Create window
+	} else { //Create window
 		gWindow = SDL_CreateWindow( "Super Mario Bros", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-	
-		if( gWindow == NULL ){
-				cout << "Window could not be created! SDL_Error: "<< SDL_GetError() << endl;
-				success = false;
-		}else{ //Get window surface
+
+		if( gWindow == NULL ) {
+			cout << "Window could not be created! SDL_Error: "<< SDL_GetError() << endl;
+			success = false;
+		} else { //Get window surface
 			gScreenSurface = SDL_GetWindowSurface( gWindow );
 		}//end if
 	}//end outer if
@@ -40,9 +40,9 @@ bool init(){
 	return success;
 }
 
-bool loadMedia(){
+bool loadMedia() {
 	bool success = true; //Loading success flag
-	
+
 	gImage = SDL_LoadBMP( "Overworld.bmp" );//Load image
 	if( gImage == NULL ){
 		success = false;
@@ -51,9 +51,9 @@ bool loadMedia(){
 	return success;
 }
 
-void close(){
+void close() {
 	SDL_FreeSurface( gImage );//Deallocate surface
-	
+
 	gImage = NULL;
 
 	SDL_DestroyWindow( gWindow ); //Destroy window
@@ -64,28 +64,28 @@ void close(){
 
 
 
-int main (int argc, char* args[]){//compatible with multiple platforms
+int main (int argc, char* args[]) {//compatible with multiple platforms.
 
 	if( !init() ){
 		printf( "Failed to initialize!\n" );
-	}else{
+	} else {
 		//Load media
 		if( !loadMedia() ){
 			printf( "Failed to load media!\n" );
-		}else{
-		
+		} else {
+
 			bool quit = false; //Main loop flag
 			SDL_Event e;//Event handler
-			
+
 			//While application is running
 			while( !quit ){
 				while( SDL_PollEvent( &e ) != 0 ){//Handle events on queue
-				
+
 					if( e.type == SDL_QUIT ){ //User requests quit
 						quit = true;
 					}
 				}
-				
+
 				SDL_BlitSurface( gImage, NULL, gScreenSurface, NULL ); //Apply the image				
 				SDL_UpdateWindowSurface( gWindow );//Update the surface
 			}//end while
