5e0578a2ba32311af51026c1e9cbe1310eccafa7
tbowers4 <tbowers4@nd.edu>
2015-03-26 12:59:12 -0400

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -6,8 +6,8 @@ class Enemy  {
     Enemy(); // non-default constructor used because all enemies will be initialized from the same spot on the screen
     bool touchTop(); // will return a true value when the player hits the top of the enemy
     bool touchSide(); // wil return a true value when any object or the player touches the enemy on either side
-    virtual void moveEnemy();  // will describe the motion of each of the enemies in derived classes
-    virtual int DeadState();  // will be used to determine if the enemy is dead and should leave the screen or is in another state
+    virtual void moveEnemy() = 0;  // will describe the motion of each of the enemies in derived classes
+    virtual int DeadState() = 0;  // will be used to determine if the enemy is dead and should leave the screen or is in another state
   
   private:
     int xpos;  // private data members for storing the location of the enemy
