7f6256dae95cc6f2cc5ccf5a5f5b702e977b92a3
Breanna <bdevorem@nd.edu>
2015-04-12 16:20:06 -0400

    Add Level class

---
+++ Level.h
@@ -0,0 +1,20 @@
+//Level.h
+//12 4 2015
+
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include <iostream>
+using namespace std;
+
+class level {
+	public:
+		level() { endLevel = false; };
+		//void EndofLevel();
+		
+	protected:
+		bool endLevel;
+};
+
+
+#endif
