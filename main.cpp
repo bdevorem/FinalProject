c51b50f6dea0f15034226ebfffa44c87df1212f4
tbowers4 <tbowers4@nd.edu>
2015-04-20 18:57:34 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,7 +23,29 @@ int main (int argc, char* args[]) {//compatible with multiple platforms.
 
 			bool quit = false; //Main loop flag
 			SDL_Event e;//Event handler
-
+			
+			SDL_Rect heroRect;
+			heroRect.x = 100;
+			heroRect.y = 290;
+			
+			SDL_Rect heroSrcRect;
+			heroSrcRect.x = 100;
+			heroSrcRect.y = 350;
+			heroSrcRect.h = 50;
+			heroSrcRect.w = 20;
+			
+			SDL_Rect srcRect;
+			srcRect.x = 100;
+			srcRect.y = 100;
+			srcRect.h = 385;
+			srcRect.w = 600;
+			
+			SDL_Rect dstRect;
+			dstRect.x = 100;
+			dstRect.y = 100;
+			dstRect.h = 1000;
+			dstRect.w = 1000;
+			
 			//While application is running
 			while( !quit ){
 				while( SDL_PollEvent( &e ) != 0 ){//Handle events on queue
@@ -37,9 +59,15 @@ int main (int argc, char* args[]) {//compatible with multiple platforms.
 						case SDL_KEYDOWN:
 							if(e.key.keysym.sym == SDLK_LEFT){
 								//user moves left
+								if(heroRect.x > 100)
+									heroRect.x--;
+								SDL_FillRect(gScreenSurface,NULL,0x000000)
 							}
 							if(e.key.keysym.sym == SDLK_RIGHT){
 								//user moves right
+								if(heroRect.x < 600)
+									heroRect.x++;
+								SDL_FillRect(gScreenSurface,NULL,0x000000)
 							}
 							if(e.key.keysym.sym == SDLK_UP){
 								//user moves up
@@ -53,7 +81,8 @@ int main (int argc, char* args[]) {//compatible with multiple platforms.
 					}//end switch
 				}//end while
 
-				SDL_BlitSurface( gImage, NULL, gScreenSurface, NULL ); //Apply the image				
+				SDL_BlitSurface( gImage, &srcRect, gScreenSurface, &dstRect ); //Apply the image	
+				SDL_BlitSurface( heroImage, &heroSrcRect, gSreenSurface, &heroRect )
 				SDL_UpdateWindowSurface( gWindow );//Update the surface
 			}//end while
 		}//end inner if
@@ -63,6 +92,8 @@ int main (int argc, char* args[]) {//compatible with multiple platforms.
 	close();
 	return 0;
 }
+
+
 
 
 
