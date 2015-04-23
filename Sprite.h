feb5049604e43cfef88bc8ebdde00bb32bc54ebc
Tallis Earl Bowers <tbowers4@fitz81.helios.nd.edu>
2015-04-23 16:06:49 -0400

    Slightly more organized sprtie class and main program.

--- Sprite.h
+++ Sprite.h
@@ -20,7 +20,7 @@ public:
 	Sprite(); //default constructor
 	void display(); //displays on screen based on outputs
 	int move(char); //moves character based on user input
-	//int jump(); //allows character to jump based on user input in a loop
+	int jump(); //allows character to jump based on user input in a loop
 	void grow(); //grows mario when he gets mushroom
 	void shrink(); //shrinks mario when he takes damage
 	int getX(); //gives x positon of sprite
@@ -39,6 +39,14 @@ public:
 	void setDirection(int);
 	int getDirection();
 	Uint32 getTime();
+	SDL_Rect getHeroRect();
+	SDL_Rect getHeroSrcRect();
+	SDL_Rect getJumpRect();
+	SDL_Rect getJumpSrcRect();
+	SDL_Rect getWalkRect();
+	SDL_Rect getWalkSrcRect();
+	SDL_Rect getSrcRect();
+	SDL_Rect getDstRect();
 
 private:
 	int x; //x position of sprite
@@ -50,6 +58,14 @@ private:
 	//int inAir; //determines if the character is in the air
 	Uint32 startTime = 0;
 	int direction; //1 means going up, 0 means going down
+	SDL_Rect heroRect;
+	SDL_Rect heroSrcRect;
+	SDL_Rect jumpRect;
+	SDL_Rect jumpSrcRect;
+	SDL_Rect walkRect;
+	SDL_Rect walkSrcRect;
+	SDL_Rect srcRect;
+	SDL_Rect dstRect;
 
 
 }; //end class definition
@@ -61,17 +77,117 @@ Sprite::Sprite() { //default constructor
 	bool large = false;
 	x = 100;
 	y = 295;
+
+	heroRect.x = 100;
+	heroRect.y = 295;
+
+	heroSrcRect.x = 0;
+	heroSrcRect.y = 0;
+	heroSrcRect.h = 32;
+	heroSrcRect.w = 24;
+
+	jumpRect.x = 0;
+	jumpRect.y = 0;
+
+	jumpSrcRect.x = 0;
+	jumpSrcRect.y = 0;
+	jumpSrcRect.h = 32;
+	jumpSrcRect.w = 32;
+
+	walkRect.x = 0;
+	walkRect.y = 0;
+
+	walkSrcRect.x = 0;
+	walkSrcRect.y = 0;
+	walkSrcRect.h = 30;
+	walkSrcRect.w = 26;
+
+	srcRect.x = 100;
+	srcRect.y = 100;
+	srcRect.h = 385;
+	srcRect.w = 600;
+
+	dstRect.x = 100;
+	dstRect.y = 100;
+	dstRect.h = 1000;
+	dstRect.w = 1000;
+
 } //end
 
 
-/*
+
 int Sprite::jump() { //jump function
+
+	if (inAir() == 0){
+		jumpUp();
+		setDirection(1);
+	}
+		resetTimer();
+		while (inAir() == 1) {
+			if (((SDL_GetTicks() - getTime()) > 110) && getDirection() == 1) {
+				jumpUp();
+				heroRect.y = NULL;
+				jumpRect.y = posY();
+				jumpRect.x = heroRect.x;
+				SDL_FillRect(gScreenSurface,NULL,0x000000);
+				SDL_BlitSurface( gImage, &srcRect, gScreenSurface, &dstRect ); //Apply the image
+				SDL_BlitSurface( jumpImage, &jumpSrcRect, gScreenSurface, &jumpRect );
+				SDL_UpdateWindowSurface( gWindow );//Update the surface
+				resetTimer();
+			} else if (((SDL_GetTicks() - getTime()) > 110) && getDirection() == 0) {
+				jumpDown();
+				heroRect.y = NULL;
+				jumpRect.y = posY();
+				jumpRect.x = heroRect.x;
+				SDL_FillRect(gScreenSurface,NULL,0x000000);
+				SDL_BlitSurface( gImage, &srcRect, gScreenSurface, &dstRect ); //Apply the image
+				SDL_BlitSurface( jumpImage, &jumpSrcRect, gScreenSurface, &jumpRect );
+				SDL_UpdateWindowSurface( gWindow );//Update the surface
+				resetTimer();
+			}
+			if (posY() <= 195) {
+				setDirection(0);
+			}
+			if (posY() > 295)
+				setY(295);
+		}
+	heroRect.y = 295;
+					
+}		
 
 int Sprite::move(char c) { //move function
 
-void Sprite::display() { //displays Sprite
+	if(c == 'l')  {
+		if(heroRect.x > 100) {
 
- */
+			heroRect.x -= 3;
+			jumpRect.x -= 3;
+		}
+			SDL_FillRect(gScreenSurface,NULL,0x000000);
+	}
+        else if(c == 'r')  {
+		if(heroRect.x < 600) {
+			walkRect.x = heroRect.x;
+			walkRect.y = heroRect.y;
+			heroRect.y = NULL;
+			SDL_FillRect(gScreenSurface,NULL,0x000000);
+			SDL_BlitSurface( gImage, &srcRect, gScreenSurface, &dstRect ); //Apply the image
+			SDL_BlitSurface( walkImage, &walkSrcRect, gScreenSurface, &walkRect );
+			SDL_UpdateWindowSurface( gWindow );//Update the surface*/
+			heroRect.x += 3;
+			heroRect.y = walkRect.y;
+			walkRect.x = NULL;
+		}
+		SDL_FillRect(gScreenSurface,NULL,0x000000);
+	}
+}
+
+void Sprite::display() { //displays Sprite
+	jumpImage == NULL;
+	SDL_BlitSurface( gImage, &srcRect, gScreenSurface, &dstRect ); //Apply the image	
+	SDL_BlitSurface( heroImage, &heroSrcRect, gScreenSurface, &heroRect );
+	SDL_UpdateWindowSurface( gWindow );//Update the surface
+}
 
 
 void Sprite::grow() { //mario becomes large in case of getting a mushroom
@@ -154,6 +270,38 @@ int Sprite::getDirection() {
 
 Uint32 Sprite::getTime() {
 	return startTime;
+}
+
+SDL_Rect Sprite::getHeroRect()  {
+	return(heroRect);
+}
+
+SDL_Rect Sprite::getHeroSrcRect()  {
+	return(heroSrcRect);
+}
+
+SDL_Rect Sprite::getJumpRect()  {
+	return jumpRect;
+}
+
+SDL_Rect Sprite::getJumpSrcRect()  {
+	return jumpSrcRect;
+}
+
+SDL_Rect Sprite::getWalkRect()  {
+	return walkRect;
+}
+
+SDL_Rect Sprite::getWalkSrcRect()  {
+	return walkSrcRect;
+}
+
+SDL_Rect Sprite::getSrcRect()  {
+	return srcRect;
+}
+
+SDL_Rect Sprite::getDstRect()  {
+	return dstRect;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -25,51 +25,6 @@ int main (int argc, char* args[]) {//compatible with multiple platforms.
 			bool quit = false; //Main loop flag
 			SDL_Event e;//Event handler
 			
-			
-			
-
-			SDL_Rect heroRect;
-			heroRect.x = 100;
-			heroRect.y = 295;
-
-			SDL_Rect heroSrcRect;
-			heroSrcRect.x = 0;
-			heroSrcRect.y = 0;
-			heroSrcRect.h = 32;
-			heroSrcRect.w = 24;
-
-			SDL_Rect jumpRect;
-			jumpRect.x = 0;
-			jumpRect.y = 0;
-
-			SDL_Rect jumpSrcRect;
-			jumpSrcRect.x = 0;
-			jumpSrcRect.y = 0;
-			jumpSrcRect.h = 32;
-			jumpSrcRect.w = 32;
-
-			SDL_Rect walkRect;
-			walkRect.x = 0;
-			walkRect.y = 0;
-
-			SDL_Rect walkSrcRect;
-			walkSrcRect.x = 0;
-			walkSrcRect.y = 0;
-			walkSrcRect.h = 30;
-			walkSrcRect.w = 26;
-
-			SDL_Rect srcRect;
-			srcRect.x = 100;
-			srcRect.y = 100;
-			srcRect.h = 385;
-			srcRect.w = 600;
-
-			SDL_Rect dstRect;
-			dstRect.x = 100;
-			dstRect.y = 100;
-			dstRect.h = 1000;
-			dstRect.w = 1000;
-
 			Sprite sp; //create sprite object
 
 			//Uint32 startTime = 0; //set timer up
@@ -86,71 +41,13 @@ int main (int argc, char* args[]) {//compatible with multiple platforms.
 
 					case SDL_KEYDOWN:
 						if(e.key.keysym.sym == SDLK_LEFT){
-							//user moves left
-							if(heroRect.x > 100) {
-
-								heroRect.x -= 3;
-								jumpRect.x -= 3;
-							}
-							SDL_FillRect(gScreenSurface,NULL,0x000000);
-						}
+							sp.move('l');  }
 						if(e.key.keysym.sym == SDLK_RIGHT){
 							//user moves right
-							if(heroRect.x < 600) {
-								walkRect.x = heroRect.x;
-								walkRect.y = heroRect.y;
-								heroRect.y = NULL;
-								SDL_FillRect(gScreenSurface,NULL,0x000000);
-								SDL_BlitSurface( gImage, &srcRect, gScreenSurface, &dstRect ); //Apply the image
-								SDL_BlitSurface( walkImage, &walkSrcRect, gScreenSurface, &walkRect );
-								SDL_UpdateWindowSurface( gWindow );//Update the surface*/
-								heroRect.x += 3;
-								heroRect.y = walkRect.y;
-								walkRect.x = NULL;
-							}
-							SDL_FillRect(gScreenSurface,NULL,0x000000);
+							sp.move('r');
 						}
 						if(e.key.keysym.sym == SDLK_UP){
-							if (sp.inAir() == 0) {
-								sp.jumpUp();
-								sp.setDirection(1);
-							}
-							sp.resetTimer();
-							while (sp.inAir() == 1) {
-								if (((SDL_GetTicks() - sp.getTime()) > 110) && sp.getDirection() == 1) {
-									sp.jumpUp();
-									heroRect.y = NULL;
-									jumpRect.y = sp.posY();
-									jumpRect.x = heroRect.x;
-									SDL_FillRect(gScreenSurface,NULL,0x000000);
-									SDL_BlitSurface( gImage, &srcRect, gScreenSurface, &dstRect ); //Apply the image
-									SDL_BlitSurface( jumpImage, &jumpSrcRect, gScreenSurface, &jumpRect );
-									SDL_UpdateWindowSurface( gWindow );//Update the surface
-									sp.resetTimer();
-								} else if (((SDL_GetTicks() - sp.getTime()) > 110) && sp.getDirection() == 0) {
-									sp.jumpDown();
-									heroRect.y = NULL;
-									jumpRect.y = sp.posY();
-									jumpRect.x = heroRect.x;
-									SDL_FillRect(gScreenSurface,NULL,0x000000);
-									SDL_BlitSurface( gImage, &srcRect, gScreenSurface, &dstRect ); //Apply the image
-									SDL_BlitSurface( jumpImage, &jumpSrcRect, gScreenSurface, &jumpRect );
-									SDL_UpdateWindowSurface( gWindow );//Update the surface
-									sp.resetTimer();
-								}
-								if (sp.posY() <= 195) {
-									sp.setDirection(0);
-								}
-								if (sp.posY() > 295)
-									sp.setY(295);
-							}
-							heroRect.y = 295;
-
-							break;
-					
-
-						}
-						heroRect.y = 295;
+							sp.jump();  }
 						if(e.key.keysym.sym == SDLK_DOWN){
 							//user moves down
 						}
@@ -162,10 +59,7 @@ int main (int argc, char* args[]) {//compatible with multiple platforms.
 					if(endGame == true) quit = true; //if the game ends (treated as 1st level for now)
 					
 				}//end while
-				jumpImage == NULL;
-				SDL_BlitSurface( gImage, &srcRect, gScreenSurface, &dstRect ); //Apply the image	
-				SDL_BlitSurface( heroImage, &heroSrcRect, gScreenSurface, &heroRect );
-				SDL_UpdateWindowSurface( gWindow );//Update the surface
+				sp.display();
 			}//end while
 			
 			//display ending of level graphics
