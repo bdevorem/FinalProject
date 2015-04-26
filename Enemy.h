a8e4c1165ae3689cbcf9c34a423d6d7bc5b21dd4
nicholascjones <njones7@nd.edu>
2015-04-26 16:43:39 -0400

    Additions to enemy class

--- Enemy.h
+++ Enemy.h
@@ -1,16 +1,69 @@
 // This is a tentative sketch for the abstract enemy class.
+#include "Sprite.h"
 
+#ifndef ENEMY_H
+#define ENEMY_H
 class Enemy  {
 
   public:
-    Enemy(); // non-default constructor used because all enemies will be initialized from the same spot on the screen
-    bool touchTop(); // will return a true value when the player hits the top of the enemy
-    bool touchSide(); // wil return a true value when any object or the player touches the enemy on either side
-    virtual void moveEnemy() = 0;  // will describe the motion of each of the enemies in derived classes
-    virtual int DeadState() = 0;  // will be used to determine if the enemy is dead and should leave the screen or is in another state
-  
+  	Enemy(int,int); // non-default constructor used because all enemies will be initialized from the same spot on the screen
+	void touch(); //determines if touching in any capacity
+  	bool getTouchTop()// will return a true value when the player hits the top of the enemy
+    	bool getTouchSide(); // wil return a true value when any object or the player touches the enemy on either side
+    	virtual void moveEnemy() = 0;  // will describe the motion of each of the enemies in derived classes
+    	virtual int DeadState() = 0;  // will be used to determine if the enemy is dead and should leave the screen or is in another state	
+	virtual void display() = 0; //pure virtual function to draw enemy
+	int getxp(); //gets xposition
+	int getyp(); //gets y position
   private:
-    int xpos;  // private data members for storing the location of the enemy
-    int ypos;
+    	int xpos;  // private data members for storing the location of the enemy
+   	int ypos;
+	bool alive;
+	bool touchTop;
+	bool touchSide;
     
 }
+#endif
+
+Enemy::Enemy(int x, int y) { //default constructor for enemy class
+	xpos = x;
+	ypos = y;
+	bool alive = true;
+	bool touchTop = false;
+	bool touchSide = false;
+}
+	
+/*
+void Enemy::touch() { //if sprite is touching top of enemy
+	mx = Sprite::getX(); //get x coordinate of sprite
+	my = Sprite::getY(); //get y coordinate of sprite
+	if ((xpos > mx-5) && (xpos < mx+5)) { //if within 5 horizontal pixels
+		if (my < (ypos + 5)) { //if touching top of enemy
+			touchTop = true;
+		} else {
+			touchSide = true;
+		}
+	}
+}
+*/
+int getxp() { //gets x position
+	return xpos;
+}
+
+int getyp() { //gets y position
+	return ypos;
+}
+
+bool Enemy::getTouchSide() { //if sprite is touching side of enemy
+	return touchSide;
+}
+
+bool Enemy::getTouchTop() { //if sprite is touching top of enemy
+	return touchTop;
+}
+			
+			
+		 
+	
+
+	
