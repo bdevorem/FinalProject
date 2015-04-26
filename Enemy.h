6e7f4b337d0fdad59a4db76ad1405c65e32b9dbf
nicholascjones <njones7@nd.edu>
2015-04-26 17:55:23 -0400

    Changes in Enemy class with additions of Goomba and Sprite classes

--- Enemy.h
+++ Enemy.h
@@ -7,20 +7,27 @@ class Enemy  {
 
   public:
   	Enemy(int,int); // non-default constructor used because all enemies will be initialized from the same spot on the screen
-	void touch(); //determines if touching in any capacity
+	void touch(Sprite); //determines if touching in any capacity
   	bool getTouchTop()// will return a true value when the player hits the top of the enemy
     	bool getTouchSide(); // wil return a true value when any object or the player touches the enemy on either side
     	virtual void moveEnemy() = 0;  // will describe the motion of each of the enemies in derived classes
-    	virtual int DeadState() = 0;  // will be used to determine if the enemy is dead and should leave the screen or is in another state	
+    	virtual int DeadState() = 0;  // will be used to determine if the enemy is dead and should leave the screen or is in another state
+	void kill(); //kills enemy
 	virtual void display() = 0; //pure virtual function to draw enemy
 	int getxp(); //gets xposition
 	int getyp(); //gets y position
+	void checkDamage(); //checks whether damage is done
+	void damage(); //subtracts 1 from enemy's hp
+	void checkAlive(); //checks whether or not enemy is alive
+	bool alive(); //gets whether or not the enemy is alive
+	int getHP(); //gets HP
   private:
     	int xpos;  // private data members for storing the location of the enemy
    	int ypos;
 	bool alive;
 	bool touchTop;
 	bool touchSide;
+	int hp;
     
 }
 #endif
@@ -28,15 +35,16 @@ class Enemy  {
 Enemy::Enemy(int x, int y) { //default constructor for enemy class
 	xpos = x;
 	ypos = y;
-	bool alive = true;
-	bool touchTop = false;
-	bool touchSide = false;
+	alive = true;
+	touchTop = false;
+	touchSide = false;
+	hp = 2;
 }
 	
-/*
-void Enemy::touch() { //if sprite is touching top of enemy
-	mx = Sprite::getX(); //get x coordinate of sprite
-	my = Sprite::getY(); //get y coordinate of sprite
+
+void Enemy::touch(Sprite mario) { //if sprite is touching top of enemy
+	mx = Sprite::mario.getX(); //get x coordinate of sprite
+	my = Sprite::mario.getY(); //get y coordinate of sprite
 	if ((xpos > mx-5) && (xpos < mx+5)) { //if within 5 horizontal pixels
 		if (my < (ypos + 5)) { //if touching top of enemy
 			touchTop = true;
@@ -45,7 +53,7 @@ void Enemy::touch() { //if sprite is touching top of enemy
 		}
 	}
 }
-*/
+
 int getxp() { //gets x position
 	return xpos;
 }
@@ -61,9 +69,32 @@ bool Enemy::getTouchSide() { //if sprite is touching side of enemy
 bool Enemy::getTouchTop() { //if sprite is touching top of enemy
 	return touchTop;
 }
+
+void Enemy::damage() {
+	hp -= 1; //lowers HP by one
+}
+
+void Enemy::checkDamage() { //checks for damage given position
+	if (touchTop == true) damage();
+}
+
+int Enemy::getHP() { //gets HP
+	return hp;
+}
 			
-			
-		 
+void Enemy::checkAlive() { //checks if enemy is alive
+	if (hp == 0) {
+		alive = false;
+	}
+}		
+
+bool Enemy::alive() { //returns whether or not enemy is alive
+	return alive;
+}
 	
+void Enemy::kill() { //actions when alive is set to false
+	//DISPLAY DEATH SPRITE AT CURRENT POSITION
+	xpos = 0; //x and y position set to (0,0)
+	ypos = 0;	//to prevent any conflict
 
 	
